818d3db8a5a71f13b07f282002c60bd3d9975caa
Flip Santos <flip.b.santos@gmail.com>
2019-10-10 17:20:02 -0300

    updating vue-cli template and adding apollo graphql

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1>A simple Reddit client made with Vue.js</h1>
-    <sidebar />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Sidebar from './components/Sidebar.vue'
-
-export default {
-  name: 'app',
-  components: {
-    // HelloWorld
-    Sidebar
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,38 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import VueApollo from 'vue-apollo';
-import { setContext } from 'apollo-link-context';
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
-Vue.use(VueApollo)
-
-const httpLink = new HttpLink({
-	uri: 'http://localhost:5000'
-})
-
-const middlewareLink = setContext(() => ({
-  headers: {}
-}));
-
-const cache = new InMemoryCache();
-
-const link = middlewareLink.concat(httpLink)
-
-const apolloClient = new ApolloClient({
-	link,
-	cache,
-	connectToDevTools: true
-})
-
-const apolloProvider = new VueApollo({
-	defaultClient: apolloClient
-})
-
 new Vue({
-	provide: apolloProvider.provide(),
-  render: h => h(App),
+  router,
+  store,
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
