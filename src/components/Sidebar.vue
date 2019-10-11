e8de74092d10dcda2288d3d9bee3f360514a1f56
Flip Santos <flip.b.santos@gmail.com>
2019-10-11 12:28:55 -0300

    Implementation of limit, before and after for queries on Reddit API

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,29 +1,43 @@
 <template>
 	<section class="sidebar-component">
-		<ul class="sidebar-component__list">
-			
-			<li 
-				class="sidebar-component__list-item"
-				v-for="article in articles"
-				:key="article.id">
-				<div class="sidebar-component__article-read">
-					{{ article.clicked }}
-				</div>
-				<div class="sidebar-component__article-subreddit">r/{{ article.subreddit }}</div>
-				<div class="sidebar-component__article-author">u/{{ article.author }} • {{ get_UTC_hours(article.created) }} hours ago</div>
-				<div class="sidebar-component__article-thumbnail">
-					<img class="sidebar-component__article-thumbnail-img" :src="article.thumbnail" :alt="article.title" />
-				</div>
-				<h3 class="sidebar-component__article-title">
-					{{ article.title }}
-				</h3>
-				<div class="sidebar-component__article-actions">
-					<a href="#">{{ article.num_comments }} comments</a>
-					<a href="#">Dismiss</a>
-				</div>
-			</li>
+		<ApolloQuery :query="require('../graphql/getTopArticles.gql')" :variables="{ limit, before, after }" >
+		
+				<template slot-scope="{ result: { loading, error, data } }">
+					<!-- Loading -->
+		      <div v-if="loading" class="loading apollo">Loading...</div>
 
-		</ul>
+		      <!-- Error -->
+		      <div v-else-if="error" class="error apollo">An error occured</div>
+
+		      <!-- Result -->
+		      <div v-else-if="data" class="result apollo">
+						<ul class="sidebar-component__list">
+							<li class="sidebar-component__list-item" v-for="article in data.getTopArticles" :key="article.id">
+								<div class="sidebar-component__article-read">
+									{{ article.clicked }}
+								</div>
+								<div class="sidebar-component__article-subreddit">r/{{ article.subreddit }}</div>
+								<div class="sidebar-component__article-author">u/{{ article.author }} • {{ get_UTC_hours(article.created) }} hours ago</div>
+								<div class="sidebar-component__article-thumbnail">
+									<img class="sidebar-component__article-thumbnail-img" :src="article.thumbnail" :alt="article.title" />
+								</div>
+								<h3 class="sidebar-component__article-title">
+									{{ article.title }}
+								</h3>
+								<div class="sidebar-component__article-actions">
+									<a href="#">{{ article.num_comments }} comments</a>
+									<a href="#">Dismiss</a>
+								</div>
+							</li>
+						</ul>
+		      </div>
+
+		      <!-- No result -->
+		      <div v-else class="no-result apollo">No result :(</div>					
+				</template>
+
+			</ul>
+    </ApolloQuery>
 	</section>
 </template>
 
@@ -41,15 +55,22 @@
 </style>
 
 <script>
-	import ARTICLE from '../graphql/Article.gql'
-	import gql from 'graphql-tag' 
+	// import GET_TOP_ARTICLES from '../graphql/GetTopArticles.gql'
+	// import gql from 'graphql-tag' 
 
 	export default {
 		name: 'Sidebar',
 
-		apollo: {
-	    articles: ARTICLE
-	  },
+		// apollo: {
+	 //    getTopArticles: GET_TOP_ARTICLES
+	 //  },
+		data () {
+			return {
+				limit: 2,
+				before: '',
+				after: ''
+			}
+		},
 
 		methods: {
 			get_UTC_hours(utc) {
@@ -59,3 +80,5 @@
 		}
 	}
 </script>
+
+
