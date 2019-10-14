e0e20da32bd975c1ec5fa33d71428381d2b359e5
Flip Santos <flip.b.santos@gmail.com>
2019-10-14 17:04:17 -0300

    adding files for deploy

---
+++ dist/precache-manifest.f4421f4bcc348552aec06536c693b379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "142c8f6e25b1e47d0635",
+    "url": "/js/chunk-vendors.f19cea62.js"
+  },
+  {
+    "revision": "c5276443ddce34dea33d",
+    "url": "/js/app.736a8d91.js"
+  },
+  {
+    "revision": "d043fdad4b2f6032b4fa3c573df6ab63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "142c8f6e25b1e47d0635",
+    "url": "/css/chunk-vendors.08e0f156.css"
+  },
+  {
+    "revision": "c5276443ddce34dea33d",
+    "url": "/css/app.6f25abfe.css"
+  }
+];
